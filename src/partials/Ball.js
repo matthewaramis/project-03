6f98c226ea8beb058384f49a2118cd9de511bc31
matthewaramis <matthew.aramis@gmail.com>
2018-11-02 10:47:47 -0700

    Add wall collision and wall bounce. Add acceleration to ball on collision.

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -10,6 +10,9 @@ export default class Ball {
   }
 
   reset() {
+    this.ax = 0.001;
+    this.ay = 0.001;
+
     this.x = this.boardWidth / 2;
     this.y = this.boardHeight / 2;
 
@@ -21,10 +24,27 @@ export default class Ball {
     this.vx = this.direction * (6 - Math.abs(this.vy));
   }
 
+  wallCollision() {
+    const hitLeft = this.x - this.radius <= 0;
+    const hitRight = this.x + this.radius >= this.boardWidth;
+    const hitTop = this.y - this.radius <= 0;
+    const hitBottom = this.y + this.radius >= this.boardHeight;
+    if (hitRight || hitLeft) {
+      this.ax *= this.ax;
+      this.vx *= -1;
+    } else if (hitTop || hitBottom) {
+      this.ay *= this.ay;
+      this.vy *= -1;
+    }
+  }
+
   render(svg) {
     this.x += this.vx;
     this.y += this.vy;
 
+    this.wallCollision();
+
+    //draw ball
     let circle = document.createElementNS(SVG_NS, "circle");
     circle.setAttributeNS(null, "cx", this.x);
     circle.setAttributeNS(null, "cy", this.y);
